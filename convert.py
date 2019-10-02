ff45540d892d3ee18475bca83dda9b908faad857
AndreM101 <56064706+AndreM101@users.noreply.github.com>
2019-10-02 15:10:11 +0800

    Add files via upload

---
+++ convert.py
@@ -0,0 +1,13 @@
+import os
+import ffmpy
+from ffmpy import FFmpeg
+
+
+def main(path, framerate):
+        for filename in os.listdir(path):
+                if (filename.endswith(".mov") or filename.endswith(".wmv") or filename.endswith(".avi")):                        
+                        os.mkdir(filename[:-4])
+                        os.chdir(path)
+                        ff = FFmpeg(inputs={filename: None}, outputs={'..\\'+filename[:-4]+'\img%04d.png': ['-vf', 'fps='+str(framerate)]})
+                        ff.run()
+                        os.chdir('..') 
