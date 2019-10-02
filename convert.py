b92b37fa232a595838492d5addc779e24f6353ee
AndreM101 <56064706+AndreM101@users.noreply.github.com>
2019-10-02 15:24:00 +0800

    update
    added a try block in case the output folder already exists

--- convert.py
+++ convert.py
@@ -6,7 +6,8 @@ from ffmpy import FFmpeg
 def main(path, framerate):
         for filename in os.listdir(path):
                 if (filename.endswith(".mov") or filename.endswith(".wmv") or filename.endswith(".avi")):                        
-                        os.mkdir(filename[:-4])
+                        try:
+                            os.mkdir(filename[:-4])
                         os.chdir(path)
                         ff = FFmpeg(inputs={filename: None}, outputs={'..\\'+filename[:-4]+'\img%04d.png': ['-vf', 'fps='+str(framerate)]})
                         ff.run()
